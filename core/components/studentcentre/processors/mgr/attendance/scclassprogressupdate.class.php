cd8c5469250709596cc1635834be7d7650b14f18
Jeff Miranda <jeffmiranda@Jeff-Mirandas-New-MacBook-Pro-2.local>
2014-08-07 10:10:18 -0600

    Enabled manual editing of ClassProgress records
    Enabled manual editing of ClassProgress records to help in tweaking the
    data import.

---
+++ core/components/studentcentre/processors/mgr/attendance/scclassprogressupdate.class.php
@@ -0,0 +1,9 @@
+<?php
+class scClassProgressUpdateProcessor extends modObjectUpdateProcessor {
+
+    public $classKey = 'scClassProgress';
+    public $languageTopics = array('studentcentre:default');
+    public $objectType = 'studentcentre.att';
+            
+}
+return 'scClassProgressUpdateProcessor';
