cc8b646cccaeea348723d3d5203d8b8b8f1c706c
Jeff Miranda <jeffmiranda@Jeff-Mirandas-New-MacBook-Pro-2.local>
2014-09-04 17:37:24 -0600

    Added getFirstLevel and getLastLevel functions

--- core/components/studentcentre/model/studentcentre/scclasslevelcategory.class.php
+++ core/components/studentcentre/model/studentcentre/scclasslevelcategory.class.php
@@ -1,2 +1,56 @@
 <?php
-class scClassLevelCategory extends xPDOSimpleObject {}
+class scClassLevelCategory extends xPDOSimpleObject {
+	
+	public function __construct(& $xpdo) {
+        parent :: __construct($xpdo);
+    }
+
+	/**
+	 * Gets the first level of the category.
+	 * Returns null if no level exists.
+	 */
+	public function getFirstLevel() {
+		
+		$level = null;
+		
+		// build query
+		$q = $this->xpdo->newQuery('scClassLevel');
+		$q->where(array(
+			'class_level_category_id' => $this->get('id')
+			,'active' => 1
+		));
+		$q->sortby('`order`', 'ASC');
+		$q->limit(1);
+		$firstLevel = $this->xpdo->getObject('scClassLevel', $q);
+		if ($firstLevel) {
+			$level = $firstLevel;
+		}
+		
+		return $level;
+	}
+	
+	/**
+	 * Gets the last level of the category.
+	 * Returns null if no level exists.
+	 */
+	public function getLastLevel() {
+		
+		$level = null;
+		
+		// build query
+		$q = $this->xpdo->newQuery('scClassLevel');
+		$q->where(array(
+			'class_level_category_id' => $this->get('id')
+			,'active' => 1
+		));
+		$q->sortby('`order`', 'DESC');
+		$q->limit(1);
+		$lastLevel = $this->xpdo->getObject('scClassLevel', $q);
+		if ($lastLevel) {
+			$level = $lastLevel;
+		}
+		
+		return $level;
+	}
+	
+}
