f1a5eedc02aa07709e0aba11036f8157f3538d60
Jeff Miranda <jeffmiranda@Jeff-Mirandas-New-MacBook-Pro-2.local>
2014-10-22 14:37:17 -0600

    Added getGrandTotalHours function
    The getGrandTotalHours function gets the student’s total hours for all
    rank categories.

--- core/components/studentcentre/model/studentcentre/scmoduser.class.php
+++ core/components/studentcentre/model/studentcentre/scmoduser.class.php
@@ -1,7 +1,31 @@
 <?php
 class scModUser extends modUser {
+    
     function __construct(xPDO & $xpdo) {
 	    parent::__construct($xpdo);
 	    $this->set('class_key','scModUser');
 	}
+	
+	/**
+	 * Gets the students total hours practiced
+	 * from all rank categories
+	 */
+	public function getGrandTotalHours() {
+		
+		$gt = null;
+		
+		$rankProgressCollection = $this->xpdo->getCollection('scClassProgress', array(
+			'student_id' => $this->get('id')
+		));
+		
+		if (!empty($rankProgressCollection)) {
+			$gt = 0;
+			foreach ($rankProgressCollection as $rankProgress) {
+				$gt = $gt + $rankProgress->get('total_hours');
+			}
+		}
+		
+		return $gt;
+	}
+
 }
