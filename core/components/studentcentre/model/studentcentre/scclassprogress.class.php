c3ce4447fcfaeedba2fc8091beb0ca7b3e98d00e
Jeff Miranda <jeffmiranda@Jeff-Mirandas-New-MacBook-Pro-2.local>
2014-11-18 16:57:29 -0700

    Added isAnniversaryMilestone function
    Added the isAnniversaryMilestone function to the classProgress object.

--- core/components/studentcentre/model/studentcentre/scclassprogress.class.php
+++ core/components/studentcentre/model/studentcentre/scclassprogress.class.php
@@ -118,6 +118,8 @@ class scClassProgress extends xPDOSimpleObject {
 		));
 		$q->sortby('`order`', 'ASC');
 		$q->limit(1);
+		//$q->prepare();
+		//error_log(print_r('SQL Statement: ' . $q->toSQL(),true));
 		$nextLevel = $this->xpdo->getObject('scClassLevel', $q);
 		if ($nextLevel) {
 			$level = $nextLevel;
@@ -180,7 +182,7 @@ class scClassProgress extends xPDOSimpleObject {
 
 	/**
 	 * Checks to see if an hourly milestone was reached
-	 * when given an hour value. If so, will return the hourly milestone.
+	 * when given an hour value. If so, will return the hourly milestone (even if the certificate already exists).
 	 * If no milestone, it will return false.
 	 * If an error occurred, NULL will be returned.
 	 * @param $hours (float)
@@ -198,7 +200,7 @@ class scClassProgress extends xPDOSimpleObject {
 			return null;
 		}
 		// get the certificate tpl type for hourly milestones
-		$certType = $modx->getObject('scCertificateType', array(
+		$certType = $this->xpdo->getObject('scCertificateType', array(
 			'name' => 'Hour'
 		));
 		if (!$certType) {
@@ -209,22 +211,161 @@ class scClassProgress extends xPDOSimpleObject {
 		$arrHourlyMilestones = explode(',', str_replace(' ', '', $hourlyMilestones));
 		foreach ($arrHourlyMilestones as $milestone) {
 			if ($gtHours < $milestone && $milestone <= ($gtHours + $hours)) {
-				// create the certificate
-				$newCertificate = $this->xpdo->newObject('scCertificate', array(
-					'student_id' => $this->xpdo->get('student_id')
-					,'certificate_type_id' => $certType->get('id')
-					,'hours' => $milestone
-					,'flag' => 1
-					,'date_created' => date('Y-m-d')
-				));
-				if (!$newCertificate->save()) {
-				   $this->xpdo->log(xPDO::LOG_LEVEL_ERROR, 'Could not save the newly created hourly certificate for studentId: ' . $this->xpdo->get('student_id'));
-				   return null;
+				if (!$this->createCertificate($this->get('student_id'), 'Hour', $milestone)) {
+					$this->xpdo->log(xPDO::LOG_LEVEL_ERROR, 'Could not create the hourly milestone certificate. studentId: ' . $this->get('student_id') . ', type: Hour, value: ' . $milestone);
+					return null;
+				} else {
+					$milestoneResult = $milestone;
 				}
-				$milestoneResult = $milestone;
 			}
 		}
 		return $milestoneResult;
 	}
+	
+	/**
+	 * Checks to see if a yearly anniversary milestone was reached
+	 * when given a date value. If so, will return the anniversary number
+	 * milestone (even if the certificate already exists).
+	 * By default, $accuracy is set to month but the user can specify
+	 * "day", "month", or "year" as the accuracy.
+	 * If no milestone, it will return false.
+	 * If an error occurred, NULL will be returned.
+	 * @param $date (date)
+	 * @param $accuracy (string)
+	 * returns mixed
+	 */
+	public function isAnniversaryMilestone($date = null, $accuracy = 'month') {
+		
+		$result = false;
+		if (is_null($date)) $date = time();
+		
+		// parse out the date, month, year
+		$dateTimeStamp = strtotime($date);
+		$d = (int) date('d', $dateTimeStamp);
+		$m = (int) date('m', $dateTimeStamp);
+		$y = (int) date('Y', $dateTimeStamp);
+
+		// get the student's start date
+		$student = $this->getOne('Student');
+		$studentProfile = $student->getOne('StudentProfile');
+		$startDate = $studentProfile->get('start_date');
+		if (empty($startDate)) {
+			// get earliest enrolled record
+			$q = $this->xpdo->newQuery('scClassEnrollment');
+			$q->where(array(
+				'student_id' => $this->get('student_id')
+				,'active' => 1
+			));
+			$q->sortby('date_created', 'ASC');
+			$q->limit(1);
+			$classEnrollment = $this->xpdo->getObject('scClassEnrollment', $q);
+			if (!$classEnrollment) {
+				$this->xpdo->log(xPDO::LOG_LEVEL_ERROR, 'Could not get the earliest class enrollment record to determine anniversary milestone.');
+				return null;
+			}
+			$startDate = strtotime($classEnrollment->get('date_created'));
+		}
+		
+		// parse out the start_date date, month, year
+		$sd = (int) date('d', $startDate);
+		$sm = (int) date('m', $startDate);
+		$sy = (int) date('Y', $startDate);
+
+		$isAnniversary = false;
+
+		// only if the year is greater than the start_date year
+		if ($y > $sy) {
+			switch (strtolower($accuracy)) {
+				case 'year':
+					$isAnniversary = true;
+				    break;
+				case 'day':
+					if ($m == $sm) {
+						if ($d >= $sd) $isAnniversary = true;
+					} elseif ($m > $sm) {
+						$isAnniversary = true;
+					}
+					break;
+				default:
+					if ($m >= $sm) $isAnniversary = true;
+			}
+		}
+		
+		// create certificate if anniversary is true
+		if ($isAnniversary) {
+			$anniversary = $y - $sy;
+			if (!$this->createCertificate($this->get('student_id'), 'Anniversary', $anniversary)) {
+				$this->xpdo->log(xPDO::LOG_LEVEL_ERROR, 'Could not create the anniversary milestone certificate. studentId: ' . $this->get('student_id') . ', type: Anniversary, value: ' . $anniversary);
+				return null;
+			} else {
+				$result = $anniversary;
+			}
+		}
+		
+		return $result;
+		
+	}
+	
+	/**
+	 * Creates a student certificate based on supplied parameters.
+	 * Will first check to see if certificate already exists.
+	 * If so, the existing certificate will be returned.
+	 * If it doesn't exist, a new certificate will be created and returned.
+	 * If creation was unsuccessful, NULL will be returned.
+	 * @param $studentId (int)
+	 * @param $type (string)
+	 * @param $value (mixed - depends on what $type is)
+	 * returns mixed
+	 */
+	public function createCertificate($studentId, $type, $value) {
+		
+		$result = null;
+		
+		// get the certificate type
+		$certType = $this->xpdo->getObject('scCertificateType', array('name' => $type));
+		if (!$certType) {
+			$this->xpdo->log(xPDO::LOG_LEVEL_ERROR, 'Could not get the certificate type object.');
+			return $result;
+		}
+		
+		// set the certificate properties
+		$certProp = array(
+			'student_id' => $studentId
+			,'certificate_type_id' => $certType->get('id')
+		);
+		switch (strtolower($type)) {
+		    case 'anniversary':
+		        $certProp['anniversary'] = $value;
+		        break;
+		    case 'hour':
+		        $certProp['hours'] = $value;
+		        break;
+		    case 'level':
+		        $certProp['level_id'] = $value;
+		        break;
+		}
+		
+		// check to see if the certificate already exists
+		$certificate = $this->xpdo->getObject('scCertificate', $certProp);
+		if (!$certificate) {
+			// Create certificate
+			$certProp['flag'] = 1;
+			$certProp['date_created'] = date('Y-m-d');
+			$newCertificate = $this->xpdo->newObject('scCertificate', $certProp);
+			if (!$newCertificate->save()) {
+				$this->xpdo->log(xPDO::LOG_LEVEL_ERROR, 'Could not save the newly created certificate for studentId: ' . $studentId);
+				return $result;
+			} else {
+				$result = $newCertificate;
+			}
+		} else {
+			// Certificate exists
+			$result = $certificate;
+		}
+		
+		return $result;
+		
+	}
+
 	
 }
