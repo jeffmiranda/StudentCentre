b2e298563c353db4030d4e2795213660143f4df7
Jeff Miranda <jeffmiranda@Jeff-Mirandas-New-MacBook-Pro-2.local>
2015-01-04 22:56:28 -0700

    Updated scTestSetup
    Updated the test setup so that the return value included all previous
    test comments

--- core/components/studentcentre/processors/mgr/attendance/sctestsetup.php
+++ core/components/studentcentre/processors/mgr/attendance/sctestsetup.php
@@ -20,7 +20,7 @@ if (!$modx->hasPermission('save_document')) return $modx->error->failure($modx->
 
 $levelId = $modx->getOption('level_id',$scriptProperties,0);
 $nextLevelId = $modx->getOption('next_level_id',$scriptProperties,0);
-error_log(print_r($scriptProperties, true));
+//error_log(print_r($scriptProperties, true));
 $studentId = $modx->getOption('student_id',$scriptProperties,0);
 $test = array();
 
@@ -56,33 +56,51 @@ if (!$techniques) {
 $test['techniques'] = $techniques;
 $test['technique_count'] = count($techniques);
 
-// Get any previous technique comments from past test (if any)
+// Begin getting the previous tests and their techniques and comments (if any)
 if ($studentId == 0) {
 	$modx->log(modX::LOG_LEVEL_ERROR, 'The Student ID was not submitted!');
 	return $modx->error->failure($modx->lexicon('studentcentre.att_err_test_load'));
 }
-
-// build the query to get the last student test and any comments
+// build the query to get all previous tests and their techniques and comments
 $graph = '{ "StudentTestTechnique":{} }';
 $q = $modx->newQuery('scStudentTest');
 $q->where(array(
 	'scStudentTest.student_id' => $studentId
 	,'scStudentTest.level_id' => $nextLevelId
 ));
-$q->sortby('scStudentTest.date_created', 'ASC');
-$q->limit(0,1);
+$q->sortby('scStudentTest.date_created', 'DESC');
+//$q->limit(0,1);
+//$q->prepare();
+//error_log(print_r('SQL Statement: ' . $q->toSQL(),true));
 // build the query obj to get the most recent comment
-$lastStudentTest = $modx->getCollectionGraph('scStudentTest', $graph, $q);
-if ($lastStudentTest) {
-	foreach ($lastStudentTest as $lst) {
-		$lastTest = array('info' => $lst);
-		$arrTechniques = array();
-		foreach ($lst->getMany('StudentTestTechnique') as $t) {
-			$arrTechniques[$t->get('technique_id')] = $t;
-		}
-		$lastTest['techniques'] = $arrTechniques;
+$prevStudentTests = $modx->getCollectionGraph('scStudentTest', $graph, $q);
+if ($prevStudentTests) {
+	// Get the most recent test and add it's info and techniques to the return array ($test)	
+	$recentTest = reset($prevStudentTests);
+	$lastTest = array('info' => $recentTest);
+	$lastTestTechniques = array();
+	foreach ($recentTest->getMany('StudentTestTechnique') as $t) {
+		$lastTestTechniques[$t->get('technique_id')] = $t;
 	}
-    $test['last_test'] = $lastTest;
+	$lastTest['techniques'] = $lastTestTechniques;
+	$test['last_test'] = $lastTest;
+	
+	// Loop through all the previous tests, and concatenate the comments for each technique
+	// and add to the return array ($test)
+	$prevTestComments = array();
+	foreach ($prevStudentTests as $tst) {
+		foreach ($tst->getMany('StudentTestTechnique') as $tec) {
+			$tecComment = $tec->get('comment');
+			if (!empty($tecComment)) {
+				if (!empty($prevTestComments[$tec->get('technique_id')])) {
+					$prevTestComments[$tec->get('technique_id')] = $tecComment . '<br>' . $prevTestComments[$tec->get('technique_id')];
+				} else {
+					$prevTestComments[$tec->get('technique_id')] = $tecComment;
+				}
+			}
+		}
+	}
+    $test['prev_comments'] = $prevTestComments;
 } else {
 	$test['last_test'] = false;
 }
