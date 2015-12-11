c51910e569e423b152a05188a60ef1c26947774e
Jeff Miranda <jeffmiranda@Jeff-Mirandas-New-MacBook-Pro.local>
2015-12-11 14:35:47 -0700

    Added functions to check journal criteria and reset journal

--- core/components/studentcentre/model/studentcentre/scjournal.class.php
+++ core/components/studentcentre/model/studentcentre/scjournal.class.php
@@ -1,2 +1,54 @@
 <?php
-class scJournal extends xPDOSimpleObject {}
+class scJournal extends xPDOSimpleObject {
+	
+	public $resetCriteria = array(
+		'belt' => 'notreceived'
+		,'certificate' => 'notreceived'
+		,'written_test_progress' => 0
+		,'test_fee' => 0
+		,'test_date' => null
+		,'pre_test_qty' => 0
+		,'active' => 0
+	);
+	
+	public function __construct(& $xpdo) {
+        parent :: __construct($xpdo);
+    }
+    
+	public function save() {
+		
+		if ($this->isJournalComplete()) { $this->resetJournal(); }
+		
+		return parent::save();
+		
+	}
+	
+	public function isJournalComplete() {
+		
+		$reset = true;
+		
+		if ($this->get('belt') != 'awarded') { $reset = false; }
+		if ($this->get('certificate') != 'awarded') { $reset = false; }
+		if ($this->get('written_test_progress') != 100) { $reset = false; }
+		if (!$this->get('test_fee')) { $reset = false; }
+		if (!$this->get('test_date')) { $reset = false; }
+				
+		return $reset;
+		
+	}
+	
+	public function resetJournal() {
+		
+		$this->fromArray($this->resetCriteria);
+		
+		$progress = $this->xpdo->getObject('scClassProgress', $this->get('class_progress_id'));
+		$nextLvl = $progress->getNextLevel();
+		if ($nextLvl) {
+			$this->set('next_level_id', $nextLvl->get('id'));
+		} else {
+			$this->set('next_level_id', 0);
+		}
+		
+	}
+	
+}
